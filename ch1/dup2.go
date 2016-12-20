a723c1c364c8b247cf77d4059429aee00f9ee0bd
sssinsi <tet643773@gmail.com>
2016-12-21 01:34:04 +0900

    practice 1.4

--- ch1/dup2.go
+++ ch1/dup2.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	filenames := make(map[string]map[string]int) //key=単語,value=ファイル名
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, filenames)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,20 +19,33 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, filenames)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			names, sep := "", ""
+			for name := range filenames[line] {
+				names += sep + name
+				sep = " "
+			}
+			fmt.Printf("%d\t%s\t%s\n", n, line, names)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, filenames map[string]map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		t := input.Text()
+		counts[t]++
+		if filenames != nil {
+			if filenames[t] == nil {
+				filenames[t] = make(map[string]int)
+			}
+			filenames[t][f.Name()]++
+		}
+
 	}
 }
