f07325e6afc9fde5e996e88d4a5dd5ef951c857a
sssinsi <tet643773@gmail.com>
2017-01-25 00:28:26 +0900

    add sample

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -1,10 +1,10 @@
 package tempconv
 
 func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FtoC(f Fahrenheit) Celsius { return Celsius((c - 32) * 5 / 9) }
+func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
-func CtoK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
-func KtoC(k Kelvin) Celsius { return Celsius(k + 273.15) }
+func CtoK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+func KtoC(k Kelvin) Celsius { return Celsius(k - 273.15) }
 
 func FtoK(f Fahrenheit) Kelvin { return CtoK(FtoC(f)) }
 func KtoF(k Kelvin) Fahrenheit { return CtoF(KtoC(k)) }
