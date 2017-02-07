8ab360656a7ea65ccafead217a0a71982be0953d
sssinsi <tet643773@gmail.com>
2017-02-08 01:13:48 +0900

    add sample

--- ch8/du4/du.go
+++ ch8/du4/du.go
@@ -9,7 +9,16 @@ import (
 	"sync"
 )
 
-var verbose = flag.Bool("v", false, "show verbose prgress messages")
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
 
 func main() {
 	//最初のディレクトリを決める。
@@ -18,11 +27,16 @@ func main() {
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+	//入力を検出するとキャンセルを伝える
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) //1バイト読み込む
+		close(done)
+	}()
 
 	//ファイルツリーを走査する
 	fileSize := make(chan int64)
 	var n sync.WaitGroup
-	for _, root := range roots {
+	for _, root = range roots {
 		n.Add(1)
 		go walkDir(root, &n, fileSize)
 	}
@@ -34,6 +48,12 @@ func main() {
 loop:
 	for {
 		select {
+		case <-done:
+			//既存のゴルーチンが完了できるようにfileSizesを空にする
+			for range fileSize {
+				//何もしない
+			}
+			return
 		case size, ok := <-fileSize:
 			if !ok {
 				break loop //fileSizeが閉じられた
@@ -56,6 +76,10 @@ func printDiskUsage(nfiles, nbytes int64) {
 // 見つかったファイルのそれぞれの大きさをfileSizesに送る
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
+	if cancelled() {
+		return
+	}
+
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			subdir := filepath.Join(dir, entry.Name())
@@ -71,7 +95,12 @@ var sema = make(chan struct{}, 20)
 
 // dirents はディレクトリdirの項目を返します。
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}        //tokenを獲得
+	select {
+	case sema <- struct{}{}: //トークンの取得
+	case <-done:
+		return nil
+	}
+
 	defer func() { <-sema }() //tokenを解放
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
